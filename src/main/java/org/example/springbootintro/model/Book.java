c7a5e9b934f0c98c99644cb74aedcd9e692f0d27
Olexander Kucherenko <amarokthegrey@gmail.com>
2026-02-16 17:30:29 +0200

    Checkstyle fix

--- src/main/java/org/example/springbootintro/model/Book.java
+++ src/main/java/org/example/springbootintro/model/Book.java
@@ -1,12 +1,18 @@
 package org.example.springbootintro.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
+
 
 @Entity
 @Table(name = "books")
