bb8aad167f0a40e4530b3353a26c0b0a46907e28
Olexander Kucherenko <amarokthegrey@gmail.com>
2026-02-16 17:27:12 +0200

    Book entity impl

---
+++ src/main/java/org/example/springbootintro/model/Book.java
@@ -0,0 +1,39 @@
+package org.example.springbootintro.model;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private String description;
+
+    private String coverImage;
+}
