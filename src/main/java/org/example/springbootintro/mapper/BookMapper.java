19fc6939fd18e966f14b0616286079bf3551a7fc
Olexander Kucherenko <amarokthegrey@gmail.com>
2026-02-20 19:32:05 +0200

    pom.xml fix, java version switched to JDK 17

--- src/main/java/org/example/springbootintro/mapper/BookMapper.java
+++ src/main/java/org/example/springbootintro/mapper/BookMapper.java
@@ -4,7 +4,6 @@ import org.example.springbootintro.dto.BookDto;
 import org.example.springbootintro.dto.CreateBookRequestDto;
 import org.example.springbootintro.model.Book;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface BookMapper {
