a737e2029f8221b0b75a46babe73fb37311e2350
Olexander Kucherenko <amarokthegrey@gmail.com>
2026-03-07 18:12:43 +0200

    renamed field in Book, removed redunant annotation in BookRepository

--- src/main/java/org/example/springbootintro/repository/BookRepository.java
+++ src/main/java/org/example/springbootintro/repository/BookRepository.java
@@ -2,9 +2,7 @@ package org.example.springbootintro.repository;
 
 import org.example.springbootintro.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
