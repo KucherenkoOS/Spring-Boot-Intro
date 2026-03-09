7d7718fd54e5d13a0c1012c179abef589bcbab30
Olexander Kucherenko <amarokthegrey@gmail.com>
2026-03-09 21:09:30 +0200

    added GlobalExceptionHandler, validation for CreateBookRequestDto & BookController, changes in SQLRestriction in Book

--- src/main/java/org/example/springbootintro/dto/CreateBookRequestDto.java
+++ src/main/java/org/example/springbootintro/dto/CreateBookRequestDto.java
@@ -1,18 +1,37 @@
 package org.example.springbootintro.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.URL;
 
 @Getter
 @Setter
 @ToString
 public class CreateBookRequestDto {
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String author;
+
+    @NotBlank
+    @Size(min = 10, max = 14)
     private String isbn;
+
+    @NotNull
+    @DecimalMin("0.0")
     private BigDecimal price;
+
+    @NotBlank
     private String description;
+
+    @NotBlank
+    @URL(message = "Cover image must be a valid URL")
     private String coverImage;
 }
