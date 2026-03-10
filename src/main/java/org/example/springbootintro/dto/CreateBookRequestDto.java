c25a463054e4700deb04739ffb7d79b25a74ee72
Olexander Kucherenko <amarokthegrey@gmail.com>
2026-03-10 11:16:26 +0200

    validation changes

--- src/main/java/org/example/springbootintro/dto/CreateBookRequestDto.java
+++ src/main/java/org/example/springbootintro/dto/CreateBookRequestDto.java
@@ -1,10 +1,10 @@
 package org.example.springbootintro.dto;
 
-import java.math.BigDecimal;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
